085e19fe5afe44f1d649416b09395d8de0061e29
Ahmed <achau1019@gmail.com>
2021-08-04 14:44:56 +0000

    Allowed form to update value of short url key and to redirect to main page

--- express_server.js
+++ express_server.js
@@ -58,6 +58,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.newURL;
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`); 
 });
