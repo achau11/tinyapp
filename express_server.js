89d7073a17ddda083143364267760e3aaa4fc2ba
Ahmed <achau1019@gmail.com>
2021-08-04 14:16:28 +0000

    Added delete buttons and functionality to remove short links

--- express_server.js
+++ express_server.js
@@ -50,8 +50,12 @@ app.get('/urls/:shortURL', (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
