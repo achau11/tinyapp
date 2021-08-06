69842d6c268c1d45005b4eb5c105764d86bb8f07
Ahmed <achau1019@gmail.com>
2021-08-06 03:30:44 +0000

    Rendered login page and created /login post request

--- express_server.js
+++ express_server.js
@@ -90,6 +90,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/login", (req, res) => {
+  let templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("urls_login", templateVars);
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
